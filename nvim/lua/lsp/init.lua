71d59d8cfdb9ffde26328f47b7cf0e899c024439
TheLeBerton <theleberton@gmail.com>
2026-05-19 14:20:39 +0200

    ADD auto-format on save

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local servers = require( "lsp.servers" )
+local servers = require("lsp.servers")
 
 local init_mason = function()
 	vim.pack.add({
@@ -10,9 +10,9 @@ local init_mason = function()
 end
 
 local start_servers = function()
-	for name, config in pairs( servers ) do
-		vim.lsp.config( name, config )
-		vim.lsp.enable( name )
+	for name, config in pairs(servers) do
+		vim.lsp.config(name, config)
+		vim.lsp.enable(name)
 	end
 end
 
@@ -20,16 +20,23 @@ M.setup = function()
 	init_mason()
 	start_servers()
 
-	vim.api.nvim_create_autocmd('LspAttach', { callback = function( args )
-		vim.lsp.inlay_hint.enable( true, { bufnr = args.buf } )
-		vim.keymap.set( "n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", { buffer = args.buf } )
-		vim.keymap.set( "n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", { buffer = args.buf } )
-		vim.keymap.set( "n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", { buffer = args.buf } )
-		vim.keymap.set( "n", "<leader>le", "<cmd>Telescope diagnostics<CR>", { buffer = args.buf } )
-		vim.keymap.set( "n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", { buffer = args.buf } )
-		vim.keymap.set( "n", "<leader>lw", "<cmd>Telescope lsp_workspace_symbols<CR>", { buffer = args.buf } )
-
-	end } )
+	vim.api.nvim_create_autocmd('LspAttach', {
+		callback = function(args)
+			vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+			vim.keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", { buffer = args.buf })
+			vim.keymap.set("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", { buffer = args.buf })
+			vim.keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", { buffer = args.buf })
+			vim.keymap.set("n", "<leader>le", "<cmd>Telescope diagnostics<CR>", { buffer = args.buf })
+			vim.keymap.set("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", { buffer = args.buf })
+			vim.keymap.set("n", "<leader>lw", "<cmd>Telescope lsp_workspace_symbols<CR>", { buffer = args.buf })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end
+			})
+		end
+	})
 end
 
 return M
