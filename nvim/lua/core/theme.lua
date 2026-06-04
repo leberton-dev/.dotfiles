fc94625a163cafb116413f520b0dcff3723bc0c7
TheLeBerton <theleberton@gmail.com>
2026-06-04 07:24:02 +0200

    FIX empty theme on startup

--- nvim/lua/core/theme.lua
+++ nvim/lua/core/theme.lua
@@ -3,8 +3,11 @@ local M = {}
 local wal_file = vim.fn.expand("~/.cache/wal/nvim-theme")
 
 function M.apply()
-	local f = io.open( wal_file, "r" )
+	local f = io.open(wal_file, "r")
 	local theme = f and f:read("*l") or "kanagawa-wave"
+	if theme == nil or theme == "" then
+		theme = "kanagawa-wave"
+	end
 	if f then f:close() end
 	vim.cmd.colorscheme(theme)
 	vim.notify("WAL theme reloaded into " .. theme, vim.log.levels.INFO)
