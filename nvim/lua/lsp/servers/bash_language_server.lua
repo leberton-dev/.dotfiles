6726efeba38a7a93e855792cc41e441cd71e6ef1
TheLeBerton <theleberton@gmail.com>
2026-06-21 07:55:16 +0200

    ADD workspace analyze for bash_ls

--- nvim/lua/lsp/servers/bash_language_server.lua
+++ nvim/lua/lsp/servers/bash_language_server.lua
@@ -2,4 +2,10 @@ return {
 	cmd = { "bash-language-server", "start" },
 	filetypes = { "bash", "sh" },
 	root_markers = { ".git" },
+	settings = {
+		bashIde = {
+			globPattern = "**/*@(.sh|..inc|.bash|.command)",
+			includeAllWorkspaceSymbols = true
+		}
+	}
 }
