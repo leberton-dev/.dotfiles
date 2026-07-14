cefbc4229bcaa6548fea9253749e5169b61007e7
TheLeBerton <theleberton@gmail.com>
2026-07-14 03:42:36 +0200

    ADD own quickfix buffer

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -30,8 +30,10 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 -- remap grep to silen grep!
+-- remap find to Find
 vim.cmd([[
 	cnoreabbrev <expr> grep getcmdtype() == ':' && getcmdline() =~# '^grep' ? "silent grep!" : "grep"
+	cnoreabbrev <expr> find getcmdtype() == ':' && getcmdline() =~# '^find' ? "Find" : "find"
 ]])
 
 -- open grep results in a new buffer full window
@@ -54,3 +56,54 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.set("n", "<CR>", "<CR>:cclose<CR>", { buffer = ev.buf })
 	end,
 })
+
+-- search all files and open a quickfix window
+vim.api.nvim_create_user_command("Find", function(opts)
+	local search_term = opts.args
+	if search_term == "" then
+		print("Please provide a search term.")
+		return
+	end
+	local files = vim.fn.systemlist("rg --files | rg " .. vim.fn.shellescape(search_term))
+	local items = {}
+	for _, file in ipairs(files) do
+		items[#items + 1] = { filename = file }
+	end
+	vim.fn.setqflist({}, "r", {
+		title = "Search Results",
+		items = items
+	})
+	vim.cmd("copen | only")
+end, { nargs = 1, complete = "file" })
+
+-- create my own style for quickfixes
+function _G.my_own_quickfix(info)
+	local qf = vim.fn.getqflist({ id = info.id, items = 0 }).items
+	local lines = {}
+	local max_width = 0
+	local max_col_width = 0
+	for _, item in ipairs(qf) do
+		local pos = string.format("%d col %d", item.lnum, item.col)
+		if #vim.fn.bufname(item.bufnr) > max_width then
+			max_width = #item.text
+		end
+		if #pos > max_col_width then
+			max_col_width = #pos
+		end
+	end
+	for i = info.start_idx, info.end_idx do
+		local item = qf[i]
+		local pos = string.format("%d col %d", item.lnum, item.col)
+		if item.lnum > 0 then
+			lines[#lines + 1] = string.format("%-" .. max_width .. "s | %-" .. max_col_width .. "s | %s",
+				vim.fn.bufname(item.bufnr),
+				pos,
+				item.text)
+		else
+			lines[#lines + 1] = string.format("%s", vim.fn.bufname(item.bufnr))
+		end
+	end
+	return lines
+end
+
+vim.o.quickfixtextfunc = "v:lua.my_own_quickfix"
