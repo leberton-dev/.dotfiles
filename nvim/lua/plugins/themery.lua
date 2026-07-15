ef8fdf692fc93ff45371602f6cac68e2192e2b62
TheLeBerton <theleberton@gmail.com>
2026-07-16 01:16:22 +0200

    ADD gruber-dark theme
    also auto-formatted the file

--- nvim/lua/plugins/themery.lua
+++ nvim/lua/plugins/themery.lua
@@ -1,14 +1,15 @@
 return {
 	src = "https://github.com/zaldih/themery.nvim",
 	deps = {
-		{ src = "https://github.com/olimorris/onedarkpro.nvim", name = "onedarkpro" },
-		{ src = "https://github.com/catppuccin/nvim.git", name = "catppuccin" },
-		{ src = "https://github.com/rebelot/kanagawa.nvim", name = "kanagawa" },
-		{ src = "https://github.com/ellisonleao/gruvbox.nvim", name = "gruvbox" },
-		{ src = "https://github.com/shaunsingh/nord.nvim", name = "nord" },
-		{ src = "https://github.com/folke/tokyonight.nvim", name = "tokyonight" },
-		{ src = "https://github.com/Mofiqul/dracula.nvim", name = "dracula" },
-		{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+		{ src = "https://github.com/olimorris/onedarkpro.nvim",     name = "onedarkpro" },
+		{ src = "https://github.com/catppuccin/nvim.git",           name = "catppuccin" },
+		{ src = "https://github.com/rebelot/kanagawa.nvim",         name = "kanagawa" },
+		{ src = "https://github.com/ellisonleao/gruvbox.nvim",      name = "gruvbox" },
+		{ src = "https://github.com/shaunsingh/nord.nvim",          name = "nord" },
+		{ src = "https://github.com/folke/tokyonight.nvim",         name = "tokyonight" },
+		{ src = "https://github.com/Mofiqul/dracula.nvim",          name = "dracula" },
+		{ src = "https://github.com/rose-pine/neovim",              name = "rose-pine" },
+		{ src = "https://github.com/blazkowolf/gruber-darker.nvim", name = "gruber-darker" },
 	},
 	config = function()
 		require("themery").setup({
