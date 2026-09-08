d3abfa69e6bedd01238ababbbb5f576f7d776bf7
TheLeBerton <theleberton@gmail.com>
2026-09-09 00:02:16 +0200

    DELETE unused scripts

--- scripts/utils/fix-lineout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ "$(hostname)" = "leberton-ubuntu" ]; then
-    sudo hda-verb /dev/snd/hwC1D0 0x1b SET_UNSOLICITED_ENABLE 0x00
-    sudo hda-verb /dev/snd/hwC1D0 0x14 SET_PIN_WIDGET_CONTROL 0x40
-fi
