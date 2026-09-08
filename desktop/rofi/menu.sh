4b8ed67cf9ac5aa0bb3d1362837f818598683242
TheLeBerton <theleberton@gmail.com>
2026-09-08 23:59:25 +0200

    DELETE desktop module

--- desktop/rofi/menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-THEME="~/.config/rofi/squared.rasi"
-
-rofi -modi combi \
-	-combi-modi "menu:~/.config/rofi/custom_menu.sh,drun" \
-	-show combi \
-	-show-icons \
-	-theme "$THEME"
--- desktop/waybar/scripts/launch_me.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pgrep -x waybar > /dev/null
-then
-	pkill waybar
-else
-	echo "Starting Waybar..."
-	waybar -c ~/.config/waybar/config.json &
-fi
